a979976a267112da6f34196919233375a383634e
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-03-07 18:33:27 +0200

    Updated comments.

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import typescript from "rollup-plugin-typescript2";
 import uglify from "rollup-plugin-uglify";
 import autoExternal from "rollup-plugin-auto-external";
 
-// `npm run build` -> `ROLLUP_WATCH` is true
-// `npm run watch` -> `ROLLUP_WATCH` is false
+// `npm run build` -> `ROLLUP_WATCH` is false
+// `npm run watch` -> `ROLLUP_WATCH` is true
 const production = !process.env.ROLLUP_WATCH;
 
 let plugins;
