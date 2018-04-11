353be5dda1e275239a987f2df1c2ce6595ff9c3c
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-04-11 21:17:51 +0300

    Fixed test tools.

--- __tests__/tests-configuration.ts
+++ __tests__/tests-configuration.ts
@@ -1,5 +1,5 @@
 import * as Enzyme from "enzyme";
-import * as EnzymeAdapter from "enzyme-adapter-react-16";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
