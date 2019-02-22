6070be2f461225cb7bbb2f8d91033df5d8601d1e
Martynas Žilinskas <martynas@quatrodev.com>
2019-02-22 15:45:46 +0200

    Cleaning old code.

--- __tests__/tests-configuration.ts
+++
@@ -1,5 +0,0 @@
-import * as Enzyme from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
--- src/helpers.ts
+++
@@ -1,4 +0,0 @@
-export const CAN_I_USE_WINDOW_LISTENERS = (typeof window !== "undefined" &&
-    window.addEventListener != null);
-
-export const ESCAPE_KEYCODE: number = 27;
